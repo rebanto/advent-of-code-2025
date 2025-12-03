c5a8a76c1c2b5a4a71d532db37aa2bb541a618ac
Rebanto Nath <rebanto.nath@gmail.com>
2025-12-02 19:02:05 -0500

    dec2 part1

---
+++ dec2/part1.py
@@ -0,0 +1,17 @@
+import re
+
+with open("input.txt") as f:
+    ranges = f.read().strip().split(",")
+
+invalids = []
+
+for rng in ranges:
+    low, high = rng.split("-")[0], rng.split("-")[1] # split the range into a low and a high number
+    
+    nums = range(int(low), int(high) + 1)
+    for n in nums:
+        n = str(n)
+        if n[:len(n) // 2] == n[len(n) // 2:]: # the problem only requires a simple check to see if the two halves are the same
+            invalids.append(int(n))
+
+print(sum(invalids))
